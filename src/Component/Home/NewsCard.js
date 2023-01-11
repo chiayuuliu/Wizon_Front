d0ccf987ab0eef2ead03941828fc9cfae7bd79fe
Doris Liu <chiayuu.liu@gmail.com>
2023-01-11 17:30:30 +0800

    擷取字串ok

--- src/Component/Home/NewsCard.js
+++ src/Component/Home/NewsCard.js
@@ -4,11 +4,10 @@ import { useNavigate } from "react-router-dom";
 
 const NewsCard = ({ title, typeIcon, redirect, description, id }) => {
   const navigate = useNavigate();
-
-
   const openInNewTab = url => {
     window.open(url, '_blank', 'noopener,noreferrer');
   };
+
   return (
     <div className='newsCard'
       onClick={() => {
@@ -26,7 +25,9 @@ const NewsCard = ({ title, typeIcon, redirect, description, id }) => {
           e.stopPropagation()
         }}>
         <h3 className="cardTitle">{title}</h3>
-        <p>{description}</p>
+        <p>
+          {description.substring(0, 50) + `...`}
+        </p>
       </div>
       <button className='viewmoreBtn'
         onClick={() => {
